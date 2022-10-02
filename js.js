49921930cde847d239c9f26a70dc1080ec9871ce
Luan Louzada <luanxdd@gmail.com>
2022-10-02 14:27:42 -0300

    adicionado botao hamburguer-menu

---
+++ js.js
@@ -0,0 +1,33 @@
+// Links ficarem ativos e sublinhados
+
+// const links = document.querySelectorAll('.header-menu a');
+
+// function ativarLink(link) {
+//     const url = location.href
+//     const href = link.href
+
+//     if (url.includes(href)){
+//         link.classList.add('ativo');
+//     }
+// }
+
+// links.forEach(ativarLink)
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+    if (event.type == 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active')
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if(active) { 
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+     
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
