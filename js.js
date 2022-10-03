012ba0794049dc1af71e511c3499ed7591f4eb77
Luan Louzada <luanxdd@gmail.com>
2022-10-03 15:19:37 -0300

    Menu scroll suave adicionado

--- js.js
+++ js.js
@@ -13,21 +13,45 @@
 
 // links.forEach(ativarLink)
 
-const btnMobile = document.getElementById('btn-mobile');
+// Menu Hamburguer
+
+const btnMobile = document.getElementById("btn-mobile");
 
 function toggleMenu(event) {
-    if (event.type == 'touchstart') event.preventDefault();
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-    const active = nav.classList.contains('active')
-    event.currentTarget.setAttribute('aria-expanded', active);
-    if(active) { 
-        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-    } else {
-        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-    }
-     
+  if (event.type == "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
 }
 
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
+
+// Scroll Suave nos Links Internos
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('#nav a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+initScrollSuave();
